ce18aac22be5e68bdf5f2debce9c58269855d747
Pedro Lacerda <pedro@inventti.com.br>
2016-06-16 15:31:22 -0300

    adding release script submodule

--- default.ps1
+++ default.ps1
@@ -1,5 +1,34 @@
+$releaseToken = '6b809a5e767f40b2af33851f7e594dfe'
+
+properties {
+	$versao
+	$url
+}
+
 Task Default -depends HelloWorld
 
 Task HelloWorld {
 	Write-Host "Hello World!" -ForegroundColor "Green"
 }
+
+Task LimparCache {
+}
+
+Task ForcarVersao {
+
+	$linkDownload = read-host "Versão: "
+
+	
+
+}
+
+Function Forcar-Versao {
+	param (
+		[string] $Versao = $(Throw New-Object System.ArgumentException "Parâmetro -Versao deve ser informado.", "Versao")
+	)
+	
+	
+	
+}
+
+Function Post
