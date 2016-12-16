a7a1dc1b8993609a4a20cc026f4687d886023298
Pedro Lacerda <pedro@inventti.com.br>
2016-12-16 09:09:18 -0200

    Removendo código desnecessário.

--- default.ps1
+++ default.ps1
@@ -1,25 +1,7 @@
 include "build\release.ps1"
 
-
 Task Default -depends HelloWorld
 
 Task HelloWorld {
 	Write-Host "Hello World!" -ForegroundColor "Green"
-}
-
-Task LimparCache {
-}
-
-Task ForcarVersao {
-
-	$linkDownload = read-host "Versão: "
-}
-
-Function Forcar-Versao {
-	param (
-		[string] $Versao = $(Throw New-Object System.ArgumentException "Parâmetro -Versao deve ser informado.", "Versao")
-	)
-	
-	
-	
 }
